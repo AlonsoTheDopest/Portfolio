ca63071cdbf5b0c2e6bde37a9aaa035cb17199ec
AlonsoTheDopest <138828524+AlonsoTheDopest@users.noreply.github.com>
2025-06-02 09:41:55 -0700

    Delete src directory

--- src/constants.js
+++
@@ -1,8 +0,0 @@
-export const EMAIL = "abgarcia2003@gmail.com"
-export const PHONE_NUM = "480-516-9199"
-export const LINKED_IN = "linkedin.com/in/garciaalonso/"
-export const GIT_HUB = "github.com/AlonsoTheDopest"
-export const ME_CAPSTONE = "ceias.nau.edu/capstone/projects/ME/2025/SP25toF25_CWCGen/"
-
-export const ICON_SIZE = 40
-export const ICON_COLOR = "#003466"
