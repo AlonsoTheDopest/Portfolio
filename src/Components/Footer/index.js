06fc7b964020d79ea09b50fa8fb88869d1cd5581
Alonso The Dopest <138828524+AlonsoTheDopest@users.noreply.github.com>
2025-08-17 01:54:25 -0700

    Added thanks to footer

--- src/Components/Footer/index.js
+++ src/Components/Footer/index.js
@@ -3,6 +3,7 @@ import "./index.css"
 export default function Header() {
     return (
         <footer>
+            <p className="thanks">Thank you for taking the time to view my portfolio!</p>
             <p>Last Updated: August 17, 2025</p>
         </footer>
     )
