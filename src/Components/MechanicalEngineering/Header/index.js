92667e4f75e0268f7a879a1fd4c00b5cba560daa
Alonso The Dopest <138828524+AlonsoTheDopest@users.noreply.github.com>
2025-08-18 19:24:38 -0700

    Changed import to correct file path

--- src/Components/MechanicalEngineering/Header/index.js
+++ src/Components/MechanicalEngineering/Header/index.js
@@ -1,4 +1,4 @@
-import Contact from '../Contact';
+import Contact from './Contact';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
