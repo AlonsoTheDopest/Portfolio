b105f977cd68a3bb5c9f244da69c1702fdd1658c
Alonso The Dopest <138828524+AlonsoTheDopest@users.noreply.github.com>
2025-08-22 23:18:53 -0700

    Renamed functions related to CardInfo to InformationCard and moved to a separate folder

---
+++ src/Components/MechanicalEngineering/Projects/Project/InformationCards/InformationCard/index.js
@@ -0,0 +1,61 @@
+    // React Bootstrap
+    import Card from 'react-bootstrap/Card'
+    import Carousel from 'react-bootstrap/Carousel';
+    import Col from 'react-bootstrap/Col';
+
+   // Other
+    import parse from 'html-react-parser'
+
+    // Turns all texts in text array as React card text component
+    function textsToReactCardTexts( texts ) {
+        return texts.map( ( text, index ) => {
+            return (
+                <li> 
+                    <Card.Text key={ index }> { parse( text ) } </Card.Text> 
+                </li>
+            )
+        })
+    }
+
+    function imagesToReactCarouselItems( images, imageAlt ) {
+        return images.map( ( image, index ) => {
+            return (
+                <Carousel.Item key={ index }>
+                    <Card.Img variant="top" src={ image } alt={ imageAlt } />
+                </Carousel.Item>
+            )
+        })
+    }
+
+// React Component that makes a card for a specific topic of a project
+export default function InformationCard( { images, imageAlt, title, texts } ) {
+    let imagesHtml = <></>
+    let textsHtml = textsToReactCardTexts( texts )
+
+    if ( images.length === 1 ) {
+        imagesHtml = <Card.Img variant="top" src={ images } alt={ imageAlt }/>
+    }
+
+    else if( images.length > 1 ) {
+        imagesHtml = (
+            <Carousel interval={ null }>
+                { imagesToReactCarouselItems( images, imageAlt ) }
+            </Carousel>
+        )
+    }
+
+    return (
+        <Col sm={ 4 }>
+            <Card className="project-info">
+                { imagesHtml }
+                <Card.Body>
+                    <Card.Title as="h3"> { title } </Card.Title>
+                    <ul>
+                        { textsHtml }
+                    </ul>
+                </Card.Body>
+            </Card>
+        </Col>
+    ) 
+    
+}  
--- src/Components/MechanicalEngineering/Projects/Project/InformationCards/index.js
+++ src/Components/MechanicalEngineering/Projects/Project/InformationCards/index.js
@@ -1,16 +1,14 @@
 // Imports
 
     // React Bootstrap
-    import Card from 'react-bootstrap/Card'
-    import Carousel from 'react-bootstrap/Carousel';
     import Row from 'react-bootstrap/Row';
-    import Col from 'react-bootstrap/Col';
+
+    // Components
+    import InformationCard from './InformationCard';
 
     // CSS
     import "./index.css"
 
-    // Other
-    import parse from 'html-react-parser'
 
 // Helper functions
 
@@ -23,60 +21,6 @@
     function areTextsEmpty( texts ) {
         return texts.length === 0
     }
-
-    // Turns all texts in text array as React card text component
-    function textsToReactCardTexts( texts ) {
-        return texts.map( ( text, index ) => {
-            return (
-                <li> 
-                    <Card.Text key={ index }> { parse( text ) } </Card.Text> 
-                </li>
-            )
-        })
-    }
-
-    function imagesToReactCarouselItems( images, imageAlt ) {
-        return images.map( ( image, index ) => {
-            return (
-                <Carousel.Item key={ index }>
-                    <Card.Img variant="top" src={ image } alt={ imageAlt } />
-                </Carousel.Item>
-            )
-        })
-    }
-
-    // React Component that makes a card for a specific topic of a project
-    function InformationCard( { images, imageAlt, title, texts } ) {
-        let imagesHtml = <></>
-        let textsHtml = textsToReactCardTexts( texts )
-
-        if ( images.length === 1 ) {
-            imagesHtml = <Card.Img variant="top" src={ images } alt={ imageAlt }/>
-        }
-
-        else if( images.length > 1 ) {
-            imagesHtml = (
-                <Carousel interval={ null }>
-                    { imagesToReactCarouselItems( images, imageAlt ) }
-                </Carousel>
-            )
-        }
-
-        return (
-            <Col sm={ 4 }>
-                <Card className="project-info">
-                    { imagesHtml }
-                    <Card.Body>
-                        <Card.Title as="h3"> { title } </Card.Title>
-                        <ul>
-                            { textsHtml }
-                        </ul>
-                    </Card.Body>
-                </Card>
-            </Col>
-        ) 
-        
-    }  
 
 
 // 
