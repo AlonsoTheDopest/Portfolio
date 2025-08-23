34eddd03300ca7fdfc78d96f0f4cdf826ddab6a7
Alonso The Dopest <138828524+AlonsoTheDopest@users.noreply.github.com>
2025-08-22 23:59:57 -0700

    Created Home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router"
+import { BrowserRouter, Routes, Route } from "react-router"
 import './App.css'
+import Home from "./Components/Home"
 import MechanicalEngineering from "./Components/MechanicalEngineering"
 import 'bootstrap/dist/css/bootstrap.min.css'
 
@@ -8,6 +9,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={ <Home /> }></Route>
         <Route path="/mechanical-engineering-portfolio" element={ <MechanicalEngineering /> }></Route>
       </Routes>
     </BrowserRouter>
